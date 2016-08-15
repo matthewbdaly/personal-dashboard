8ebfee9002fbd48ffde28b7e5b344ff94193951c
Matthew Daly <matthewbdaly@gmail.com>
2016-08-15 16:45:00 +0100

    Added test for feed widget

---
+++ test/components/feedwidget.js
@@ -0,0 +1,19 @@
+import TestUtils from 'react-addons-test-utils';
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+import FeedWidget from '../../js/components/feedwidget';
+import {expect} from 'chai';
+
+const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate} = TestUtils;
+
+describe('Feed Widget', () => {
+  it('renders the Feed widget', () => {
+    const url = "http://feeds.bbci.co.uk/news/rss.xml?edition=uk"
+    const component = renderIntoDocument(
+      <FeedWidget feed={url} />
+    );
+    const feed = findDOMNode(component.refs.feed);
+    expect(feed).to.be.ok;
+    expect(feed.textContent).to.contain(url);
+  });
+});
