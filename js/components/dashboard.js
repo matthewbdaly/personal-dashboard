5345479d23be02f868fc068e47723bc7a951a3b3
Matthew Daly <matthewbdaly@gmail.com>
2016-08-15 21:06:37 +0100

    Amended dashboard

--- js/components/dashboard.js
+++ js/components/dashboard.js
@@ -9,7 +9,7 @@ export default React.createClass({
         <h1 ref="title">{this.props.title}</h1>
         <div className="wrapper">
           <ClockWidget />
-          <FeedWidget feed="http://feeds.bbci.co.uk/news/rss.xml?edition=uk" size="5" />
+          <FeedWidget feed="http://feeds.bbci.co.uk/news/rss.xml?edition=uk" size="5" delay="60" />
         </div>
       </div>
     );
